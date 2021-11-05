284bf88f1da23d6d22347028fb9bf79b08bd5ede
samuel.rison <samuelrison@yahoo.fr>
2021-11-05 21:17:02 +0100

    Adding some code, still working on some lines

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,7 @@
 import requests
 import sys
-import re
 
 from LoLCoaching.config import config
-import utils
 
 # RIOT API Related informations
 BASE_URL_EUW = "https://euw1.api.riotgames.com"
@@ -18,10 +16,100 @@ BY_PUUID = "by-puuid"
 api_key = config.get_api_key()
 
 
+class Data:
+    def __init__(self, teams_data, specific_data, data_at_x):
+        self.teams_data_set = teams_data
+        self.specific_data_set = specific_data
+        self.data_at_x_set = data_at_x
+        self.data = {}
+        self.win_data = {}
+        self.lose_data = {}
+        for elem in teams_data:
+            self.data[elem] = 0.0
+            self.win_data[elem] = 0.0
+            self.lose_data[elem] = 0.0
+        for elem in specific_data:
+            self.data[elem] = 0.0
+            self.win_data[elem] = 0.0
+            self.lose_data[elem] = 0.0
+        for elem in data_at_x:
+            self.data[elem] = 0.0
+            self.win_data[elem] = 0.0
+            self.lose_data[elem] = 0.0
+        self.count = 0
+        self.win_count = 0
+        self.lose_count = 0
+
+    def add_data(self, match):
+        for elem in self.data:
+            if elem in self.teams_data_set:
+                self.data[elem] += match[elem]
+            elif elem in self.specific_data_set:
+                self.data[elem] += match[elem]
+            elif elem in self.data_at_x_set:
+                for key in match:
+                    if "stat_at_" in key:
+                        self.data[elem] += match[key][elem]
+        self.count += 1
+        if match["win"] is True:
+            for elem in self.win_data:
+                if elem in self.teams_data_set:
+                    self.win_data[elem] += match[elem]
+                elif elem in self.specific_data_set:
+                    self.win_data[elem] += match[elem]
+                elif elem in self.data_at_x_set:
+                    for key in match:
+                        if "stat_at_" in key:
+                            self.win_data[elem] += match[key][elem]
+            self.win_count += 1
+        else:
+            for elem in self.lose_data:
+                if elem in self.teams_data_set:
+                    self.lose_data[elem] += match[elem]
+                elif elem in self.specific_data_set:
+                    self.lose_data[elem] += match[elem]
+                elif elem in self.data_at_x_set:
+                    for key in match:
+                        if "stat_at_" in key:
+                            self.lose_data[elem] += match[key][elem]
+            self.lose_count += 1
+
+    def make_average(self):
+        for elem in self.data:
+            if self.count > 0:
+                self.data[elem] = self.data[elem] / self.count
+            if self.win_count > 0:
+                self.win_data[elem] = self.win_data[elem] / self.win_count
+            if self.lose_count > 0:
+                self.lose_data[elem] = self.lose_data[elem] / self.lose_count
+
+    def print(self):
+        print(self.data, self.count)
+        print(self.win_data, self.win_count)
+        print(self.lose_data, self.lose_count)
+
+
+def get_team_data_list():
+    return {"damageShare": "totalDamageDealtToChampions", "goldShare": "goldEarned"}
+
+
+def get_specific_data_list():
+    return {"goldEfficiency": "goldEfficiency", "deaths": "deaths", "k+a": "k+a"}
+
+
+def get_at_x_data_list():
+    return {"totalGold": "totalGold"}
+
+
 def check_position(position: str) -> bool:
     if position in ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"]:
         return True
     return False
+
+
+def calculate_gold_efficiency(goldEarned: int, damageDealt: int) -> float:
+    player_efficiency = damageDealt / goldEarned * 100
+    return player_efficiency
 
 
 def get_player_puuid(username: str) -> str:
@@ -71,6 +159,19 @@ def get_match_stats(match_id: str) -> dict:
     return data
 
 
+def get_extra_data_at_minute(data: dict, minute: int) -> dict:
+    target_frame = 0
+    for frame in data["frames"]:
+        if 60000 * (minute + 1) < frame["timestamp"]:
+
+    for frame in data["frames"]:
+        if frame["timestamp"] < 60000 * (minute + 1):
+            events = frame["events"]
+            for event in events:
+                if event["type"] == "CHAMPION_KILL":
+
+
+
 def get_stats_at_minute(match_id: str, minute: int) -> dict:
     url = f"{BASE_URL_EUROPE}/{GAME}/{MATCH_API}/{match_id}/timeline?api_key={api_key}"
     resp = requests.get(url)
@@ -78,6 +179,7 @@ def get_stats_at_minute(match_id: str, minute: int) -> dict:
     if "info" not in data_json:
         raise Exception(f"No information retrieved for match {match_id}")
     data = data_json["info"]
+    data = get_extra_data_at_minute(data, minute)
     for frame in data["frames"]:
         if 60000 * (minute + 1) > frame["timestamp"] > 60000 * minute:
             return {"timestamp": frame["timestamp"], "data": frame["participantFrames"]}
@@ -100,18 +202,6 @@ def aggregate_data(match_data: dict, minutes_data=None, team_data=None) -> dict:
     return match_data
 
 
-def get_team_data_list():
-    return {"damageShare": "totalDamageDealtToChampions", "goldShare": "goldEarned"}
-
-
-def get_specific_data_list():
-    return {}
-
-
-def get_at_x_data_list():
-    return {"totalGold": "totalGold"}
-
-
 def create_team_stats(match_data: dict, data_list: dict) -> dict:
     team_data = {"100": {}, "200": {}}
     for elem in data_list:
@@ -123,18 +213,31 @@ def create_team_stats(match_data: dict, data_list: dict) -> dict:
     return team_data
 
 
+def get_player_data(match_data: dict, username: str, share_team_stat=None) -> dict:
+    for player in match_data["participants"]:
+        if player["summonerName"] == username:
+            if share_team_stat is not None:
+                for elem in share_team_stat:
+                    player[elem] = player[share_team_stat[elem]] /\
+                                   match_data["teams"][int(player["teamId"]/100-1)][share_team_stat[elem]] * 100
+            player["goldEfficiency"] = calculate_gold_efficiency(player["goldEarned"],
+                                                                 player["totalDamageDealtToChampions"])
+            player["k+a"] = player["kills"] + player["assists"]
+            return player
+    return {}
+
+
 def get_set_data(match_list, username, position):
-    match_stats = []
     team_data_list = get_team_data_list()
-    specific_data_list = get_specific_data_list()
-    at_x_data_list = get_at_x_data_list()
+    player_stats = []
     for match in match_list:
         data = get_match_stats(match)
         if is_match_valid(data) is True and check_player_position(data, name=username, position=position) is True:
             data_at_15 = get_stats_at_minute(match, 15)
             team_data = create_team_stats(data, team_data_list)
-            match_stats.append(aggregate_data(data, minutes_data=[data_at_15], team_data=team_data))
-    return match_stats
+            match_data = aggregate_data(data, minutes_data=[data_at_15], team_data=team_data)
+            player_stats.append(get_player_data(match_data, username, team_data_list))
+    return player_stats
 
 
 def get_match_list(username: str, count: int) -> list:
@@ -143,14 +246,40 @@ def get_match_list(username: str, count: int) -> list:
     return match_list
 
 
+def get_champion_list(player_stats: list) -> list:
+    champion_list = []
+    for game in player_stats:
+        if game["championName"] not in champion_list:
+            champion_list.append(game["championName"])
+    return champion_list
+
+
 def main():
     if len(sys.argv) < 3:
         return 0
     username = sys.argv[1]
     position = sys.argv[2]
-    match_list = get_match_list(username, 30)
-    match_data = get_set_data(match_list, username, position)
-    print(match_data)
+    match_list = get_match_list(username, 40)
+    player_stats = get_set_data(match_list, username, position)
+    champion_list = get_champion_list(player_stats)
+    specific_data = get_specific_data_list()
+    team_data = get_team_data_list()
+    at_x_data = get_at_x_data_list()
+    average_data = Data(team_data, specific_data, at_x_data)
+    champion_data = {}
+    for champion in champion_list:
+        champion_data[champion] = Data(team_data, specific_data, at_x_data)
+    for game in player_stats:
+        average_data.add_data(game)
+        for champion in champion_list:
+            if game["championName"] == champion:
+                champion_data[champion].add_data(game)
+    average_data.make_average()
+    average_data.print()
+    for champion in champion_list:
+        champion_data[champion].make_average()
+        print(champion, ":")
+        champion_data[champion].print()
 
 
 main()
@@ -183,70 +312,70 @@ main()
 
 
 
-### SETUP PHASE ###
-
-# Put the username of the player you want to see the stats.
-# ingame_position is TOP, JUNGLE, MIDDLE, BOTTOM or UTILITY
-username = "MiniWolskys"
-ingame_position = "MIDDLE"
-
-# Stats to retrieve
-stats_to_retrieve_share = {
-    "damageShare": "totalDamageDealtToChampions",
-    "goldShare": "goldEarned",
-}
-
-stats_to_retrieve_team = {
-    "heraldKilled": "riftHerald",
-    "baronKilled": "baron"
-}
-
-stats_to_retrieve = {
-    "visionScore": "visionScore",
-    "controlScore": "timeCCingOthers"
-}
-
-# Total stats
-position_match_count = 0
-average_stats = {}
-for elem in stats_to_retrieve_share:
-    average_stats[elem] = 0
-for elem in stats_to_retrieve_team:
-    average_stats[elem] = 0
-for elem in stats_to_retrieve:
-    average_stats[elem] = 0
-average_gold_efficiency = 0
-
-# Stats in wins
-win_number = 0
-win_stats = {}
-for elem in stats_to_retrieve_share:
-    win_stats[elem] = 0
-for elem in stats_to_retrieve_team:
-    win_stats[elem] = 0
-for elem in stats_to_retrieve:
-    win_stats[elem] = 0
-win_average_gold_efficiency = 0
-
-# Stats in losses
-lose_number = 0
-loss_stats = {}
-for elem in stats_to_retrieve_share:
-    loss_stats[elem] = 0
-for elem in stats_to_retrieve_team:
-    loss_stats[elem] = 0
-for elem in stats_to_retrieve:
-    loss_stats[elem] = 0
-lose_average_gold_efficiency = 0
-
-# championList is a dictionary of all champions played at given position with detailed stats for the champion.
-championList = {}
-
-# Getting all stats for match
-# match_id: {"final_stats": end_game_stats, "15_stats": stats_at_15}
-match_stats = {}
-
-### GAME STATS ###
+# ### SETUP PHASE ###
+#
+# # Put the username of the player you want to see the stats.
+# # ingame_position is TOP, JUNGLE, MIDDLE, BOTTOM or UTILITY
+# username = "MiniWolskys"
+# ingame_position = "MIDDLE"
+#
+# # Stats to retrieve
+# stats_to_retrieve_share = {
+#     "damageShare": "totalDamageDealtToChampions",
+#     "goldShare": "goldEarned",
+# }
+#
+# stats_to_retrieve_team = {
+#     "heraldKilled": "riftHerald",
+#     "baronKilled": "baron"
+# }
+#
+# stats_to_retrieve = {
+#     "visionScore": "visionScore",
+#     "controlScore": "timeCCingOthers"
+# }
+#
+# # Total stats
+# position_match_count = 0
+# average_stats = {}
+# for elem in stats_to_retrieve_share:
+#     average_stats[elem] = 0
+# for elem in stats_to_retrieve_team:
+#     average_stats[elem] = 0
+# for elem in stats_to_retrieve:
+#     average_stats[elem] = 0
+# average_gold_efficiency = 0
+#
+# # Stats in wins
+# win_number = 0
+# win_stats = {}
+# for elem in stats_to_retrieve_share:
+#     win_stats[elem] = 0
+# for elem in stats_to_retrieve_team:
+#     win_stats[elem] = 0
+# for elem in stats_to_retrieve:
+#     win_stats[elem] = 0
+# win_average_gold_efficiency = 0
+#
+# # Stats in losses
+# lose_number = 0
+# loss_stats = {}
+# for elem in stats_to_retrieve_share:
+#     loss_stats[elem] = 0
+# for elem in stats_to_retrieve_team:
+#     loss_stats[elem] = 0
+# for elem in stats_to_retrieve:
+#     loss_stats[elem] = 0
+# lose_average_gold_efficiency = 0
+#
+# # championList is a dictionary of all champions played at given position with detailed stats for the champion.
+# championList = {}
+#
+# # Getting all stats for match
+# # match_id: {"final_stats": end_game_stats, "15_stats": stats_at_15}
+# match_stats = {}
+#
+# ### GAME STATS ###
 
 
         # team_1_stats = {}
